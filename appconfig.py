5a7635ec0e5a05f31a32f0fbabc99efacd446311
Eugene <eugene@camel-dev.local>
2023-02-12 16:34:19 -0500

    added http client sample

--- appconfig.py
+++ appconfig.py
@@ -1,22 +1,29 @@
 import configparser
 from pathlib import Path
-
+import sys
 
 
 class AppConfig:
-    path2cfg = '/Users/eugene/projects/ML/smoke-tester/smoke-tester.cfg'
-    if not Path(path2cfg).is_file():
-        raise Exception('Specified file does not exist')
- 
     config = configparser.ConfigParser()
+    envId = None
+    env = None
+    envUrl = None
 
-    def __init__(self, envId) -> None: 
-        self.config.read(self.path2cfg)
-        result = list(filter(lambda section: (section == envId), self.config.sections()))
-        print(result)
+    def __init__(self, envId) -> None:
+        path2cfg = Path(sys.argv[0]).name.replace(
+            '.py', '.ini')  # use config file with extension .ini
+        if not Path(path2cfg).is_file():
+            raise Exception('The app config file does not exist: ' + path2cfg)
+
+        self.config.read(path2cfg)
+        result = list(
+            filter(lambda section: (section == envId), self.config.sections()))
+
         if not result:
-            raise Exception('There is no configuration for ' + envId+ ' in ' + self.path2cfg)
+            raise Exception('There is no configuration for ' + envId + ' in ' +
+                            path2cfg)
 
-
-    # def getInstance(self) -> configparser.ConfigParser():
-    #     return self.config
+        self.envId = envId
+        self.env = self.config[envId]
+        self.envUrl = self.env['protocol'] + '//' + self.env['host']
+        print('... selected env: ' + self.envId)
--- smoke-tester.py
+++ smoke-tester.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 import appconfig
 import appargs
+import http.client
 
 
 args = appargs.AppArgs().args
-config = appconfig.AppConfig(args.env).config
+cfg = appconfig.AppConfig(args.env)
 
-print (config[args.env].get('protocol'))
+print('... env host: ' + cfg.env['host'])
+
+conn = http.client.HTTPConnection(cfg.env['host'])
+conn.set_debuglevel(10)
+conn.request("POST", "/session")
+response  = conn.getresponse()
+print(response.status, response.reason)
+print(response.read())
+conn.close()
+
+# url = 'https://github.com/session'
+# data = {"eventType": "AAS_PORTAL_START", "data": {"uid": "hfe3hf45huf33545", "aid": "1", "vid": "1"}}
+# params = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103', 'format': 'xml', 'platformId': 1}
+# requests.post(url, params=params, json=data)
+
+
